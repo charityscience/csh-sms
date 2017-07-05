0d76a4f922c56c428f2dbeffb910597c79c2c0c1
Marcus A. Davis <marcus.a.davis24@gmail.com>
2017-07-05 12:34:08 -0700

    Add simple index and detail view for Contact

--- management/urls.py
+++ management/urls.py
@@ -2,12 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
-# app_name = 'management'
+app_name = 'management'
 
-# urlpatterns = [
-#     url(r'^$', views.IndexView.as_view(), name='index')
-# ]
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 ]
