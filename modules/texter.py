0d7a43a136e0ad3aa640f1e8eb5d59520b500cfc
peterhurford <peter@peterhurford.com>
2017-07-11 14:18:41 -0500

    update HSPSMS documentation link

--- modules/texter.py
+++ modules/texter.py
@@ -1,4 +1,4 @@
-# Here is the HSPSMS documentation: https://drive.google.com/drive/u/0/folders/0B3lpvu9ww5atVW9HZUE0b1FBb28
+# Here is the HSPSMS documentation: https://docs.google.com/document/d/1mACLXYFvnjkWJ3oQimN2sscy1G-VGSQAG0zkWhkLYHY/edit?usp=sharing
 # If you need to login, the site is: http://sms.hspsms.com 
 
 def send_text(message):
