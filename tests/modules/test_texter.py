522f61300b32339fdc2a7c503863ec89be6606a9
Marcus A. Davis <marcus.a.davis24@gmail.com>
2017-07-31 12:57:49 -0700

    Test Texter read_inbox returns dict

--- tests/modules/test_texter.py
+++ tests/modules/test_texter.py
@@ -1,13 +1,13 @@
-# TODO test after the library is ready
 from django.test import TestCase
 from modules.texter import Texter
-from modules.textlocalwrapper import TextLocal
 
 class TexterGetInboxesTests(TestCase):
 	def create_texter(self):
 		return Texter()
 
 
-	def test_get_inboxes(self):
-		# TODO: Implement for real
-		pass
+	def test_read_inbox(self):
+		texter = Texter()
+		inbox_dict = texter.read_inbox()
+
+		self.assertIsInstance(inbox_dict, dict)
