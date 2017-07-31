059a27af294ae9edd4f3b2c70f73d580e8ca71e2
Marcus A. Davis <marcus.a.davis24@gmail.com>
2017-07-31 11:09:21 -0700

    Get messages from inbox

--- modules/textlocalwrapper.py
+++ modules/textlocalwrapper.py
@@ -1,21 +1,28 @@
 import json
 from urllib import request, parse
 from cshsms.settings import TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID
-from modules.texter import Texter
 
 class TextLocal(object):
 	def __init__(self, apikey, primary_id):
 		self.apikey = apikey
 		self.primary_id = primary_id
 
-	def all_inboxes(self):
+	def get_all_inboxes(self):
 		params = {'apikey': self.apikey}
 		inboxes_url = 'https://api.textlocal.in/get_inboxes/?'
 		f = request.urlopen(inboxes_url + parse.urlencode(params))
 		return json.loads(f.read().decode('latin1')), f.code
 
-	def primary_inbox(self):
+	def get_primary_inbox(self):
 		params = {'apikey': self.apikey, 'inbox_id': self.primary_id}
 		messages_url = 'https://api.textlocal.in/get_messages/?'
 		f = request.urlopen(messages_url + parse.urlencode(params))
 		return json.loads(f.read().decode('latin1')), f.code
+
+	def get_primary_inbox_messages(self):
+		inbox_and_code = self.get_primary_inbox()
+		inbox = inbox_and_code[0]
+		return inbox['messages']
+
+	def is_message_new(self, message):
+		return True if message['isNew'] == 'True'.lower() else False
--- tests/modules/test_textlocalwrapper.py
+++ tests/modules/test_textlocalwrapper.py
@@ -3,21 +3,37 @@ from django.test import TestCase
 from modules.textlocalwrapper import TextLocal
 
 
-class TextLocalGetInboxesTests(TestCase):
+class TextLocalInboxesTests(TestCase):
 	def test_create_object(self):
 		textlocal = TextLocal(TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID)
 		self.assertIsInstance(textlocal, TextLocal)
 		
 	def test_get_all_inboxes(self):
 		textlocal = TextLocal(TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID)
-		response, status = textlocal.all_inboxes()
+		response, status = textlocal.get_all_inboxes()
 		self.assertIsInstance(response, dict)
 		self.assertGreaterEqual(int(response['num_inboxes']), 1)
 
 	def test_get_primary_inbox(self):
 		textlocal = TextLocal(TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID)
-		primary_inbox, status = textlocal.primary_inbox()
+		primary_inbox, status = textlocal.get_primary_inbox()
 		self.assertTrue(primary_inbox)
 		self.assertIsInstance(primary_inbox, dict)
 		self.assertTrue(primary_inbox['status'])
 		self.assertEqual(primary_inbox['status'], 'success')
+
+	def test_get_primary_inbox_messages(self):
+		textlocal = TextLocal(TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID)
+		primary_inbox_messages = textlocal.get_primary_inbox_messages()
+		self.assertTrue(primary_inbox_messages)
+		self.assertIsInstance(primary_inbox_messages, list)
+
+	def test_is_message_new(self):
+		textlocal = TextLocal(TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID)
+		new_message = {'number': '910987654321', 'message': 'New message',
+			'isNew': 'true'}
+		old_message = {'number': '910987654321', 'message': 'Old message',
+			'isNew': 'None'}
+
+		self.assertTrue(textlocal.is_message_new(new_message))
+		self.assertFalse(textlocal.is_message_new(old_message))
