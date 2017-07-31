df50ccd8744792acf736207fdb835fe66eabf0fb
Marcus A. Davis <marcus.a.davis24@gmail.com>
2017-07-30 18:30:32 -0700

    Get primary inbox

--- modules/textlocalwrapper.py
+++ modules/textlocalwrapper.py
@@ -4,11 +4,18 @@ from cshsms.settings import TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID
 from modules.texter import Texter
 
 class TextLocal(object):
-	def __init__(self, apikey):
+	def __init__(self, apikey, primary_id):
 		self.apikey = apikey
+		self.primary_id = primary_id
 
 	def all_inboxes(self):
 		params = {'apikey': self.apikey}
 		inboxes_url = 'https://api.textlocal.in/get_inboxes/?'
 		f = request.urlopen(inboxes_url + parse.urlencode(params))
+		return json.loads(f.read().decode('latin1')), f.code
+
+	def primary_inbox(self):
+		params = {'apikey': self.apikey, 'inbox_id': self.primary_id}
+		messages_url = 'https://api.textlocal.in/get_messages/?'
+		f = request.urlopen(messages_url + parse.urlencode(params))
 		return json.loads(f.read().decode('latin1')), f.code
--- tests/modules/test_textlocalwrapper.py
+++ tests/modules/test_textlocalwrapper.py
@@ -5,18 +5,19 @@ from modules.textlocalwrapper import TextLocal
 
 class TextLocalGetInboxesTests(TestCase):
 	def test_create_object(self):
-		textlocal = TextLocal("fakekey")
+		textlocal = TextLocal(TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID)
 		self.assertIsInstance(textlocal, TextLocal)
 		
 	def test_get_all_inboxes(self):
-		textlocal = TextLocal(TEXTLOCAL_API)
+		textlocal = TextLocal(TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID)
 		response, status = textlocal.all_inboxes()
 		self.assertIsInstance(response, dict)
 		self.assertGreaterEqual(int(response['num_inboxes']), 1)
 
 	def test_get_primary_inbox(self):
-		pass
-
-
-	def test_get_inbox_by_id(self):
-		pass
+		textlocal = TextLocal(TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID)
+		primary_inbox, status = textlocal.primary_inbox()
+		self.assertTrue(primary_inbox)
+		self.assertIsInstance(primary_inbox, dict)
+		self.assertTrue(primary_inbox['status'])
+		self.assertEqual(primary_inbox['status'], 'success')
