41b76c667990cf89ed9d5bc80dfe594eacf8535e
Marcus A. Davis <marcus.a.davis24@gmail.com>
2017-08-01 16:34:15 -0700

    Create Hspsms class

--- modules/hspsmswrapper.py
+++ modules/hspsmswrapper.py
@@ -1,1 +1,7 @@
-# Implement for real
+from cshsms.settings import HSPSMS_API, HSPSMS_USERNAME, HSPSMS_SENDERNAME
+
+class Hspsms(object):
+	def __init__(self, apikey, username, sendername):
+		self.apikey = apikey
+		self.username = username
+		self.sendername = sendername
--- tests/modules/test_hspsmswrapper.py
+++ tests/modules/test_hspsmswrapper.py
@@ -1,1 +1,9 @@
-# Implement for real
+from cshsms.settings import HSPSMS_API, HSPSMS_USERNAME, HSPSMS_SENDERNAME
+from django.test import TestCase
+from modules.hspsmswrapper import Hspsms
+
+class HspsmsSendingTests(TestCase):
+	def test_create_objects(self):
+		hspsms = Hspsms(apikey=HSPSMS_API, username=HSPSMS_USERNAME, sendername=HSPSMS_SENDERNAME)
+		self.assertIsInstance(hspsms, Hspsms)
+
