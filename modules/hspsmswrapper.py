17810b6fda69da4a1b85148a4851bb99399fc86b
peterhurford <peter@peterhurford.com>
2017-08-18 15:14:51 -0500

    add mock test for test_send_transactional_message

--- modules/hspsmswrapper.py
+++ modules/hspsmswrapper.py
@@ -4,20 +4,20 @@ from six import u
 from six.moves.urllib import request, parse
 
 class Hspsms(object):
-	def __init__(self, apikey, username, sendername):
-		self.apikey = apikey
-		self.username = username
-		self.sendername = sendername
+    def __init__(self, apikey, username, sendername):
+        self.apikey = apikey
+        self.username = username
+        self.sendername = sendername
 
-	def send_transactional_message(self, message, phone_number):
-		send_url = 'http://sms.hspsms.com/sendSMS?'
-		data = parse.urlencode({'username': self.username,
+    def send_transactional_message(self, message, phone_number):
+        send_url = 'http://sms.hspsms.com/sendSMS?'
+        data = parse.urlencode({'username': self.username,
                                 'message': message,
                                 'sendername': self.sendername,
                                 'smstype': 'TRANS',
                                 'numbers': phone_number,
                                 'apikey': self.apikey})
-		data = data.encode('utf-8')
-		requester = request.Request(send_url)
-		f = request.urlopen(requester, data)
-		return u(json.loads(f.read().decode('latin1')))
+        data = data.encode('utf-8')
+        requester = request.Request(send_url)
+        f = request.urlopen(requester, data)
+        return u(json.loads(f.read().decode('latin1')))
--- tests/modules/test_hspsmswrapper.py
+++ tests/modules/test_hspsmswrapper.py
@@ -1,8 +1,28 @@
-from cshsms.settings import HSPSMS_API, HSPSMS_USERNAME, HSPSMS_SENDERNAME
+import json
+import mock
+from mock import patch
+
 from django.test import TestCase
+
 from modules.hspsmswrapper import Hspsms
 
+
 class HspsmsSendingTests(TestCase):
-	def test_create_objects(self):
-		hspsms = Hspsms(apikey=HSPSMS_API, username=HSPSMS_USERNAME, sendername=HSPSMS_SENDERNAME)
-		self.assertIsInstance(hspsms, Hspsms)
+    def test_create_objects(self):
+        hspsms = Hspsms(apikey='mock_key',
+                        username='mock_user',
+                        sendername='mock_sendername')
+        self.assertIsInstance(hspsms, Hspsms)
+
+
+    @patch("modules.hspsmswrapper.request")
+    def test_send_transactional_message(self, mock_request):
+        class MockResponse():
+            def read(self):
+                return json.dumps({'message': 'yay'}).encode('latin1')
+        mock_request.urlopen.return_value = MockResponse()
+        hspsms = Hspsms(apikey='mock_key',
+                        username='mock_user',
+                        sendername='mock_sendername')
+        response = hspsms.send_transactional_message(message='Test', phone_number='0000000')
+        self.assertEqual(response['message'], 'yay')
