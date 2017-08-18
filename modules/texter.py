565f8fa75a671a2402793be76ec61a2bf87ac913
peterhurford <peter@peterhurford.com>
2017-08-18 14:45:38 -0500

    formatting

--- modules/texter.py
+++ modules/texter.py
@@ -10,6 +10,9 @@ class Texter(object):
 	    return num_message_dict
 
 	def send(self, message, phone_number):
-		hspsms = Hspsms(apikey=HSPSMS_API, username=HSPSMS_USERNAME, sendername=HSPSMS_SENDERNAME)
-		send_status = hspsms.send_transactional_message(message=message, phone_number=phone_number)
+		hspsms = Hspsms(apikey=HSPSMS_API,
+                        username=HSPSMS_USERNAME,
+                        sendername=HSPSMS_SENDERNAME)
+		send_status = hspsms.send_transactional_message(message=message,
+                                                        phone_number=phone_number)
 		return send_status
