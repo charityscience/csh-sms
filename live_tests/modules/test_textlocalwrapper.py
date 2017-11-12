0a5deeeba026e7da828218748d8d60ac3e474d2d
marcus <marcus.a.davis24@gmail.com>
2017-11-12 11:26:05 -0800

    Update TextLocal object creation in during live tests

--- live_tests/modules/test_textlocalwrapper.py
+++ live_tests/modules/test_textlocalwrapper.py
@@ -2,16 +2,20 @@ import time
 import logging
 
 from django.test import TestCase
+from datetime import datetime
 
-from cshsms.settings import TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID, TEXTLOCAL_PHONENUMBER
+from cshsms.settings import TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID, TEXTLOCAL_SENDERNAME, \
+                            TEXTLOCAL_PHONENUMBER
 from modules.textlocalwrapper import TextLocal
 from modules.texter import Texter
-from modules.i18n import hindi_remind, msg_placeholder_child
+from modules.i18n import hindi_remind, msg_placeholder_child, msg_already_sub
 
 
 class TextLocalInboxesTests(TestCase):
     def test_create_object(self):
-        textlocal = TextLocal(TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID)
+        textlocal = TextLocal(apikey=TEXTLOCAL_API,
+                                primary_id=TEXTLOCAL_PRIMARY_ID,
+                                sendername=TEXTLOCAL_SENDERNAME)
         self.assertIsInstance(textlocal, TextLocal)
         
     def test_get_all_inboxes(self):
@@ -21,7 +25,9 @@ class TextLocalInboxesTests(TestCase):
         self.assertGreaterEqual(int(response['num_inboxes']), 1)
 
     def test_get_primary_inbox(self):
-        textlocal = TextLocal(TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID)
+        textlocal = TextLocal(apikey=TEXTLOCAL_API,
+                                primary_id=TEXTLOCAL_PRIMARY_ID,
+                                sendername=TEXTLOCAL_SENDERNAME)
         primary_inbox = textlocal.get_primary_inbox()
         self.assertTrue(primary_inbox)
         self.assertIsInstance(primary_inbox, dict)
@@ -29,13 +35,17 @@ class TextLocalInboxesTests(TestCase):
         self.assertEqual(primary_inbox['status'], 'success')
 
     def test_get_primary_inbox_messages(self):
-        textlocal = TextLocal(TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID)
+        textlocal = TextLocal(apikey=TEXTLOCAL_API,
+                                primary_id=TEXTLOCAL_PRIMARY_ID,
+                                sendername=TEXTLOCAL_SENDERNAME)
         primary_inbox_messages = textlocal.get_primary_inbox_messages()
         self.assertTrue(primary_inbox_messages)
         self.assertIsInstance(primary_inbox_messages, list)
 
     def test_new_messages_by_number(self):
-        textlocal = TextLocal(TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID)
+        textlocal = TextLocal(apikey=TEXTLOCAL_API,
+                                primary_id=TEXTLOCAL_PRIMARY_ID,
+                                sendername=TEXTLOCAL_SENDERNAME)
         logging.info("sending text - English")
         texter = Texter()
         texter.send(message="This is a live test message.", phone_number=TEXTLOCAL_PHONENUMBER)
@@ -46,7 +56,9 @@ class TextLocalInboxesTests(TestCase):
         self.assertTrue("This is a live test message." in new_message_dict[0])
 
     def test_new_messages_by_number_hindi(self):
-        textlocal = TextLocal(TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID)
+        textlocal = TextLocal(apikey=TEXTLOCAL_API,
+                                primary_id=TEXTLOCAL_PRIMARY_ID,
+                                sendername=TEXTLOCAL_SENDERNAME)
         logging.info("sending text - Hindi")
         texter = Texter()
         hindi_message = hindi_remind() + ' ' + msg_placeholder_child('Hindi')
