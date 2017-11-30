b43f22a8d76a8d0171f714a8397f3a6f44e434c0
marcus <marcus.a.davis24@gmail.com>
2017-11-30 10:15:05 -0800

    Test for retrieving api send messages

--- live_tests/modules/test_textlocalwrapper.py
+++ live_tests/modules/test_textlocalwrapper.py
@@ -42,6 +42,14 @@ class TextLocalInboxesTests(TestCase):
         self.assertTrue(primary_inbox_messages)
         self.assertIsInstance(primary_inbox_messages, list)
 
+    def test_api_send_history_messages(self):
+        textlocal = TextLocal(apikey=TEXTLOCAL_API,
+                                primary_id=TEXTLOCAL_PRIMARY_ID,
+                                sendername=TEXTLOCAL_SENDERNAME)
+        api_send_history_messages = textlocal.get_api_send_history_messages()
+        self.assertTrue(api_send_history_messages)
+        self.assertIsInstance(api_send_history_messages, list)
+
     def test_new_messages_by_number(self):
         textlocal = TextLocal(apikey=TEXTLOCAL_API,
                                 primary_id=TEXTLOCAL_PRIMARY_ID,
@@ -55,4 +63,19 @@ class TextLocalInboxesTests(TestCase):
         new_message_dict = textlocal.new_messages_by_number()
         self.assertTrue(msg_already_sub("English") in new_message_dict[0][0])
         self.assertIsInstance(new_message_dict[0][1], datetime)
+
+    def test_new_api_send_messages_by_number(self):
+        textlocal = TextLocal(apikey=TEXTLOCAL_API,
+                                primary_id=TEXTLOCAL_PRIMARY_ID,
+                                sendername=TEXTLOCAL_SENDERNAME)
+        logging.info("sending text - English")
+        texter = Texter()
+        texter.send(message=msg_already_sub("English"), phone_number=TEXTLOCAL_PHONENUMBER)
+        logging.info("sleeping three minutes before reading text")
+        time.sleep(180)
+        logging.info("reading text")
+        new_message_dict = textlocal.new_api_send_messages_by_number()
+        message_list_by_phonenumber = new_message_dict[TEXTLOCAL_PHONENUMBER]
+        self.assertTrue(msg_already_sub("English") in message_list_by_phonenumber[0][0])
+        self.assertIsInstance(message_list_by_phonenumber[0][1], datetime)
 
