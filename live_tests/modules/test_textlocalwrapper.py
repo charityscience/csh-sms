9e435fde8c2fd6a78d48c3b77c1169fbc8b937c2
marcus <marcus.a.davis24@gmail.com>
2017-11-30 11:28:56 -0800

    Test TextLocal message sending in English

--- live_tests/modules/test_textlocalwrapper.py
+++ live_tests/modules/test_textlocalwrapper.py
@@ -8,7 +8,8 @@ from cshsms.settings import TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID, TEXTLOCAL_SENDE
                             TEXTLOCAL_PHONENUMBER
 from modules.textlocalwrapper import TextLocal
 from modules.texter import Texter
-from modules.i18n import hindi_remind, msg_placeholder_child, msg_already_sub
+from modules.i18n import hindi_remind, msg_placeholder_child, msg_already_sub, \
+                            msg_unsubscribe
 
 
 class TextLocalInboxesTests(TestCase):
@@ -67,3 +68,25 @@ class TextLocalInboxesTests(TestCase):
         self.assertTrue(msg_already_sub("English") in message_list_by_phonenumber[0][0])
         self.assertIsInstance(message_list_by_phonenumber[0][1], datetime)
 
+    def test_send_message_english(self):
+        textlocal = TextLocal(apikey=TEXTLOCAL_API,
+                                primary_id=TEXTLOCAL_PRIMARY_ID,
+                                sendername=TEXTLOCAL_SENDERNAME)
+        logging.info("sending transactional message - English")
+        fr = textlocal.send_message(message=msg_unsubscribe("English"),
+                                    phone_numbers=TEXTLOCAL_PHONENUMBER)
+        texter = Texter()
+        logging.info("sleeping three minutes before reading the inbox")
+        time.sleep(180)
+        logging.info("reading the inbox")
+        new_message_dict = textlocal.new_api_send_messages_by_number()
+        message_list_by_phonenumber = new_message_dict[TEXTLOCAL_PHONENUMBER]
+        self.assertTrue(fr)
+        self.assertIsInstance(fr, dict)
+        self.assertTrue(fr['message'])
+        self.assertEqual(fr['status'], "success")
+        self.assertTrue(fr["messages"])
+        self.assertTrue(fr["messages"][0]["recipient"])
+        self.assertEqual(fr["messages"][0]["recipient"], int(TEXTLOCAL_PHONENUMBER))
+        self.assertTrue(msg_unsubscribe("English") in message_list_by_phonenumber[0][0])
+        self.assertIsInstance(message_list_by_phonenumber[0][1], datetime)
