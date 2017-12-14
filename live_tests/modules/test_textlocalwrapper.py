d9246c8dc2bb2d20e0e04ecac7b296b76a158af8
marcus <marcus.a.davis24@gmail.com>
2017-12-14 15:56:16 -0800

    Test sending gujarati message with TextLocal

--- live_tests/modules/test_textlocalwrapper.py
+++ live_tests/modules/test_textlocalwrapper.py
@@ -9,7 +9,8 @@ from cshsms.settings import TEXTLOCAL_API, TEXTLOCAL_PRIMARY_ID, TEXTLOCAL_SENDE
 from modules.textlocalwrapper import TextLocal
 from modules.texter import Texter
 from modules.i18n import hindi_remind, msg_placeholder_child, msg_already_sub, \
-                            msg_unsubscribe
+                            msg_unsubscribe, six_week_reminder_seven_days, \
+                            gujarati_placeholder_name
 
 
 class TextLocalInboxesTests(TestCase):
@@ -113,4 +114,28 @@ class TextLocalInboxesTests(TestCase):
         self.assertEqual(fr["messages"][0]["recipient"], int(TEXTLOCAL_PHONENUMBER))
         response_unicode_encoded = textlocal.response_unicode_encoder(message_list_by_phonenumber[0][0])
         self.assertEqual(msg_unsubscribe("Hindi"), response_unicode_encoded)
+        self.assertIsInstance(message_list_by_phonenumber[0][1], datetime)
+
+    def test_send_message_gujarati(self):
+        textlocal = TextLocal(apikey=TEXTLOCAL_API,
+                                primary_id=TEXTLOCAL_PRIMARY_ID,
+                                sendername=TEXTLOCAL_SENDERNAME)
+        logging.info("sending transactional message - Gujarati")
+        fr = textlocal.send_message(message=six_week_reminder_seven_days("Gujarati").format(name=gujarati_placeholder_name()),
+                                    phone_numbers=TEXTLOCAL_PHONENUMBER)
+        texter = Texter()
+        logging.info("sleeping three minutes before reading the inbox")
+        time.sleep(180)
+        logging.info("reading the inbox")
+        new_message_dict = textlocal.new_api_send_messages_by_number()
+        message_list_by_phonenumber = new_message_dict[TEXTLOCAL_PHONENUMBER]
+        self.assertTrue(fr)
+        self.assertIsInstance(fr, dict)
+        self.assertTrue(fr['message'])
+        self.assertEqual(fr['status'], "success")
+        self.assertTrue(fr["messages"])
+        self.assertTrue(fr["messages"][0]["recipient"])
+        self.assertEqual(fr["messages"][0]["recipient"], int(TEXTLOCAL_PHONENUMBER))
+        response_unicode_encoded = textlocal.response_unicode_encoder(message_list_by_phonenumber[0][0])
+        self.assertEqual(six_week_reminder_seven_days("Gujarati").format(name=gujarati_placeholder_name()), response_unicode_encoded)
         self.assertIsInstance(message_list_by_phonenumber[0][1], datetime)
